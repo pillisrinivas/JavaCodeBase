888ccf0d0f1bea33baac134a00346a67a9a48f04
pilli srinivas <pillisrinivas@gmail.com>
2019-01-19 12:09:22 +0530

    Removed CoreJava project - duplicate source code

--- CoreJava/src/com/java/concepts/collections/comparable/MovieType.java
+++
@@ -1,7 +0,0 @@
-package com.java.concepts.collections.comparable;
-
-public interface MovieType {
-	double getRating();
-	int getYear();
-	String getName();
-}
--- CoreJava/src/com/java/concepts/collections/set/CustomerSortType.java
+++
@@ -1,5 +0,0 @@
-package com.java.concepts.collections.set;
-
-public interface CustomerSortType {
-
-}
